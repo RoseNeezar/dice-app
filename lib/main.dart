96958d8d0e83b34c41e2ac4f0655a0f6cc659a4d
Biceps <rforlyffe@gmail.com>
2019-10-14 02:43:26 +0800

    random number

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int temp = 1;
+  int temp1 = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  temp = new Random().nextInt(6) + 1;
+                  print('left = $temp');
+                });
+              },
+              child: Image.asset('images/dice$temp.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  temp1 = new Random().nextInt(6) + 1;
+                  print('right = $temp1');
+                });
+              },
+              child: Image.asset('images/dice$temp1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
